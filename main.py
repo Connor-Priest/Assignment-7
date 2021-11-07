3bbf5281b948927bc3eafae0767c20d88776cee3
Connor-Priest <cpriest965@g.rwu.edu>
2021-11-07 12:40:36 -0500

    Ran a cmd command and read the data using re

--- main.py
+++ main.py
@@ -3,14 +3,21 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+import os
+import re
+import subprocess
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+def regex():
+    subprocesscheck = subprocess.Popen("ipconfig", shell=True, stdout=subprocess.PIPE)
+    subprocess_return = subprocesscheck.stdout.read()
+
+    search = re.search(rb'(?<=IPv4).*\w+', subprocess_return)
+    print(search)
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    regex()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
